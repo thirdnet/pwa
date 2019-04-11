3364adf46681fc1dc5549eb6b6921d00a92ad301
thirdnet <49368440+thirdnet@users.noreply.github.com>
2019-04-11 18:34:42 +0900

    PWA登録

---
+++ service-worker.js
@@ -0,0 +1,12 @@
+// service-worker.js
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Install');
+});
+
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activate');
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch');
+});
